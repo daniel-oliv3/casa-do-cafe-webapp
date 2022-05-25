6a65ad46e3132fb56440cdaa348c114e6044cc7b
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-25 10:00:34 -0300

    Facility section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { FacilityComponent } from './facility/facility.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    FacilityComponent
   ],
   imports: [
     BrowserModule,
